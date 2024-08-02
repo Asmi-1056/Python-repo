12321269033dd63431869708c59b5bb18377a5e2
Asmi Purarkar <asmipurarkar@gmail.com>
2024-08-03 02:31:43 +0530

    Assignment6_Py

---
+++ Assignment 6/exceptions.py
@@ -0,0 +1,14 @@
+#Asmi Purarkar
+
+#exceptions.py
+
+class CustomException(Exception):   #base class for other custom exceptions
+    pass
+
+class InvalidMenuItemError(CustomException):
+    #Raised when the menu item is invalid.
+    pass
+
+class InsufficientQuantityError(CustomException):
+    #Raised when there is not enough quantity of a menu item.
+    pass
