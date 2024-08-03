c0c121d886ad770e2d627c9edd6601fee1fbdcfe
Asmi Purarkar <asmipurarkar@gmail.com>
2024-08-03 05:39:22 +0530

    Assignment8_Py

---
+++ Assignment 8/Exception.py
@@ -0,0 +1,11 @@
+# Asmi Purarkar
+
+#Exception.py file
+
+# Custom exception class
+import os
+import pandas as pd 
+class DataCleaningError(Exception):
+    def __init__(self, message="Error during data cleaning"):
+        self.message = message
+        super().__init__(self.message)
